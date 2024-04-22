445c5638df2ace2693107ff655e50e271456a4fb
tarun261003 <tarunmangalampalli@gmail.com>
2024-04-22 12:27:08 +0530

    Final Afternoon commit

--- Patterns/box.py
+++ Patterns/box.py
@@ -1,6 +1,8 @@
 def return_number(i,j):
     if i==0 or i==2*n-2 or j==0 or   j==2*n-2:
         return n
+    elif i==n-1 and j==n-1:
+        return 1
     else:
         return '*'
 n=int(input('Enter the size:'))
