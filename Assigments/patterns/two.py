d494a37cb80d3da6c09ea45ca135fec4f0740f5c
tarun261003 <tarunmangalampalli@gmail.com>
2024-04-22 09:30:01 +0530

    Day 4 Morning commit

--- Assigments/patterns/two.py
+++ Assigments/patterns/two.py
@@ -1,6 +1,6 @@
 #diamond pattern
 n=int(input('Enter the size of pattern:'))
-for i in range(n):
+for i in range(1,n):
     print('*'*i,end='\n')
-for i in range(n,-1,-1):
+for i in range(n,0,-1):
     print('*'*i,end='\n')
