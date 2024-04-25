a91357862222660f4e6d7b5c62857347867f14bc
tarun261003 <tarunmangalampalli@gmail.com>
2024-04-25 15:45:15 +0530

    Evening Commit

--- Class/oops/four.py
+++ Class/oops/four.py
@@ -20,5 +20,5 @@ if __name__=="__main__":
     z2=complex(c,d)
     com=Complex(z1,z2)
     print(f'The addition of "{z1}" and "{z2}" is "{com.add_complex()}"')
-    print(f'The subtraction of "{z1}" and {z2} is "{com.subtract_complex()}')
+    print(f'The subtraction of "{z1}" and "{z2}" is "{com.subtract_complex()}')
     print(f'The product of "{z1}" and "{z2}" is "{com.product_complex()}"')
